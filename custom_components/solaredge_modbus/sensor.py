fe34bce7ce7a76f218b85ecc3dc5fea346ec47ec
Erik A <erik@arenhill.com>
2020-02-22 16:45:39 +0100

    fix: try reduce cpu load by adding sleep

--- custom_components/solaredge_modbus/sensor.py
+++ custom_components/solaredge_modbus/sensor.py
@@ -71,6 +71,7 @@ class SolarEdgeModbusSensor(Entity):
         task = loop.create_task(self.modbus_loop())
 
     async def modbus_loop(self):
+	sleep(0.005)
         while True:
             try:
 		        
